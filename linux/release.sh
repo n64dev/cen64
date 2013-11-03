54a6cffcbf1c1c3998c1fb6ddf84d66a61ad4fed
Devin <thatnerd2049@gmail.com>
2013-11-03 18:47:02 -0500

    Adding Linux Compile Scripts

---
+++ linux/release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#Devin's Cen64 Linux Compile Script.
+
+GetVersionFromFile()
+{
+	VERSION=`cat $1 | tr "\n" ' ' | sed s/.*VERSION.*=\ // `
+}
+
+	if [ -f /etc/redhat-release ] ; then
+		DIST='RedHat'
+	elif [ -f /etc/debian_version ] ; then
+		DIST="Debian"
+	fi
+
+if [ "${DIST}" = "RedHat" ] ; then
+sudo yum install git libglfw-devel libalut-devel
+elif [ "${DIST}" = "Debian" ] ; then
+sudo apt-get install git libglfw-dev libalut-dev
+fi
+
+cd ../
+git submodule init
+git pull
+git submodule update
+make clean && make
