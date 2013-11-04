b8a75ed7d66d6450bf517d429511b6f022a56f0d
ThatNerd2049 <ThatNerd2049@gmail.com>
2013-11-03 20:21:13 -0500

    Update debug.sh

--- linux/debug.sh
+++ linux/debug.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-#Devin's Cen64 Linux Compile Script.
 
-	if [ -f /etc/redhat-release ] ; then
-		sudo yum install git libglfw-devel libalut-devel
-	elif [ -f /etc/debian_version ] ; then
-		sudo apt-get install git libglfw-dev libalut-dev
-	fi
+if [ -f /etc/redhat-release ] ; then
+	sudo yum install git libglfw-devel libalut-devel
+elif [ -f /etc/debian_version ] ; then
+	sudo apt-get install git libglfw-dev libalut-dev
+fi
 
 cd ../
 git submodule init
