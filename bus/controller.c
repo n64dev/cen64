5dce6e0ea7b87b27f62b6421fb2c2ebacf70a623
Tyler Stachecki <tstache1@binghamton.edu>
2014-07-31 12:35:06 -0400

    Cut down on initialization code size.

--- bus/controller.c
+++ bus/controller.c
@@ -24,6 +24,15 @@
 #include "vr4300/cpu.h"
 #include "vr4300/interface.h"
 
+#define NUM_MAPPINGS 14
+
+struct bus_controller_mapping {
+  memory_rd_function read;
+  memory_wr_function write;
+  uint32_t address;
+  uint32_t length;
+};
+
 // Releases resources acquired by the bus component.
 void bus_cleanup(struct bus_controller *bus) {
   destroy_memory_map(bus->map);
@@ -31,50 +40,50 @@ void bus_cleanup(struct bus_controller *bus) {
 
 // Initializes the bus component.
 int bus_init(struct bus_controller *bus) {
-  if ((bus->map = create_memory_map(14)) == NULL)
+  unsigned i;
+
+  static const struct bus_controller_mapping mappings[NUM_MAPPINGS] = {
+    {read_ai_regs, write_ai_regs, AI_REGS_BASE_ADDRESS, AI_REGS_ADDRESS_LEN},
+    {read_dp_regs, write_dp_regs, DP_REGS_BASE_ADDRESS, DP_REGS_ADDRESS_LEN},
+    {read_mi_regs, write_mi_regs, MI_REGS_BASE_ADDRESS, MI_REGS_ADDRESS_LEN},
+    {read_pi_regs, write_pi_regs, PI_REGS_BASE_ADDRESS, PI_REGS_ADDRESS_LEN},
+    {read_ri_regs, write_ri_regs, RI_REGS_BASE_ADDRESS, RI_REGS_ADDRESS_LEN},
+    {read_si_regs, write_si_regs, SI_REGS_BASE_ADDRESS, SI_REGS_ADDRESS_LEN},
+    {read_sp_regs, write_sp_regs, SP_REGS_BASE_ADDRESS, SP_REGS_ADDRESS_LEN},
+    {read_vi_regs, write_vi_regs, VI_REGS_BASE_ADDRESS, VI_REGS_ADDRESS_LEN},
+
+    {read_cart_rom, write_cart_rom, ROM_CART_BASE_ADDRESS, ROM_CART_ADDRESS_LEN},
+    {read_pif_ram, write_pif_ram, PIF_RAM_BASE_ADDRESS, PIF_RAM_ADDRESS_LEN},
+    {read_pif_rom, write_pif_rom, PIF_ROM_BASE_ADDRESS, PIF_ROM_ADDRESS_LEN},
+    {read_rdram_regs, write_rdram_regs, RDRAM_REGS_BASE_ADDRESS, RDRAM_REGS_ADDRESS_LEN},
+    {read_sp_regs2, write_sp_regs2, SP_REGS2_BASE_ADDRESS, SP_REGS2_ADDRESS_LEN},
+    {read_sp_mem, write_sp_mem, SP_MEM_BASE_ADDRESS, SP_MEM_ADDRESS_LEN},
+  };
+
+  void *instances[NUM_MAPPINGS] = {
+    bus->ai,
+    bus->rdp,
+    bus->vr4300,
+    bus->pi,
+    bus->ri,
+    bus->si,
+    bus->rsp,
+    bus->vi,
+
+    bus->pi,
+    bus->si,
+    bus->si,
+    bus->ri,
+    bus->rsp,
+    bus->rsp
+  };
+
+  if ((bus->map = create_memory_map(NUM_MAPPINGS)) == NULL)
     return -1;
 
-  map_address_range(bus->map, AI_REGS_BASE_ADDRESS, AI_REGS_ADDRESS_LEN,
-    bus->ai, read_ai_regs, write_ai_regs);
-
-  map_address_range(bus->map, DP_REGS_BASE_ADDRESS, DP_REGS_ADDRESS_LEN,
-    bus->rdp, read_dp_regs, write_dp_regs);
-
-  map_address_range(bus->map, MI_REGS_BASE_ADDRESS, MI_REGS_ADDRESS_LEN,
-    bus->vr4300, read_mi_regs, write_mi_regs);
-
-  map_address_range(bus->map, PI_REGS_BASE_ADDRESS, PI_REGS_ADDRESS_LEN,
-    bus->pi, read_pi_regs, write_pi_regs);
-
-  map_address_range(bus->map, PIF_RAM_BASE_ADDRESS, PIF_RAM_ADDRESS_LEN,
-    bus->si, read_pif_ram, write_pif_ram);
-
-  map_address_range(bus->map, PIF_ROM_BASE_ADDRESS, PIF_ROM_ADDRESS_LEN,
-    bus->si, read_pif_rom, write_pif_rom);
-
-  map_address_range(bus->map, RDRAM_REGS_BASE_ADDRESS, RDRAM_REGS_ADDRESS_LEN,
-    bus->ri, read_rdram_regs, write_rdram_regs);
-
-  map_address_range(bus->map, RI_REGS_BASE_ADDRESS, RI_REGS_ADDRESS_LEN,
-    bus->ri, read_ri_regs, write_ri_regs);
-
-  map_address_range(bus->map, ROM_CART_BASE_ADDRESS, ROM_CART_ADDRESS_LEN,
-    bus->pi, read_cart_rom, write_cart_rom);
-
-  map_address_range(bus->map, SI_REGS_BASE_ADDRESS, SI_REGS_ADDRESS_LEN,
-    bus->si, read_si_regs, write_si_regs);
-
-  map_address_range(bus->map, SP_MEM_BASE_ADDRESS, SP_MEM_ADDRESS_LEN,
-    bus->rsp, read_sp_mem, write_sp_mem);
-
-  map_address_range(bus->map, SP_REGS_BASE_ADDRESS, SP_REGS_ADDRESS_LEN,
-    bus->rsp, read_sp_regs, write_sp_regs);
-
-  map_address_range(bus->map, SP_REGS2_BASE_ADDRESS, SP_REGS2_ADDRESS_LEN,
-    bus->rsp, read_sp_regs2, write_sp_regs2);
-
-  map_address_range(bus->map, VI_REGS_BASE_ADDRESS, VI_REGS_ADDRESS_LEN,
-    bus->vi, read_vi_regs, write_vi_regs);
+  for (i = 0; i < NUM_MAPPINGS; i++)
+    map_address_range(bus->map, mappings[i].address, mappings[i].length,
+      instances[i], mappings[i].read, mappings[i].write);
 
   return 0;
 }
@@ -89,6 +98,7 @@ int bus_read_word(struct bus_controller *bus,
 
   else if ((node = resolve_mapped_address(bus->map, address)) == NULL) {
     debug("bus_read_word: Failed to access: 0x%.8X\n", address);
+
     *word = 0x00000000U;
     return 0;
   }
@@ -106,6 +116,7 @@ int bus_write_word(struct bus_controller *bus,
 
   else if ((node = resolve_mapped_address(bus->map, address)) == NULL) {
     debug("bus_write_word: Failed to access: 0x%.8X\n", address);
+
     return 0;
   }
 
