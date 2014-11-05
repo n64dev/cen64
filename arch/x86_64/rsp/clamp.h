6a0604eaca4fe430db7a930e1f369a0c82cfdc35
Tyler Stachecki <tstache1@binghamton.edu>
2014-11-05 15:09:16 -0500

    Fix the RSP accumulator clamping algorithm.

--- arch/x86_64/rsp/clamp.h
+++ arch/x86_64/rsp/clamp.h
@@ -7,25 +7,20 @@
 
 static inline __m128i rsp_clamp_lo(__m128i acc_lo,
   __m128i acc_md, __m128i acc_hi, __m128i zero) {
-  __m128i mid_sign, neg_check, neg_val, pos_val, use_val_mask;
+  __m128i clamp_match_mask, sign_match_mask;
+  __m128i lo, hi, md_clamped, clamp_value;
 
-  neg_check = _mm_cmplt_epi16(acc_hi, zero);
-  mid_sign = _mm_srai_epi16(acc_md, 15);
+  lo = _mm_unpacklo_epi16(acc_md, acc_hi);
+  hi = _mm_unpackhi_epi16(acc_md, acc_hi);
+  md_clamped = _mm_packs_epi32(lo, hi);
 
-  neg_val = _mm_and_si128(neg_check, acc_hi);
-  use_val_mask = _mm_and_si128(mid_sign, neg_val);
-  neg_val = _mm_and_si128(use_val_mask, acc_lo);
+  clamp_match_mask = _mm_cmpeq_epi16(md_clamped, acc_md);
+  clamp_value = _mm_srai_epi16(md_clamped, 15);
 
-  use_val_mask = _mm_or_si128(acc_hi, mid_sign);
-  use_val_mask = _mm_cmpeq_epi16(use_val_mask, zero);
-  pos_val = _mm_and_si128(use_val_mask, acc_lo);
+  sign_match_mask = _mm_cmpeq_epi16(clamp_value, acc_hi);
+  clamp_match_mask = _mm_and_si128(clamp_match_mask, sign_match_mask);
+  clamp_value = _mm_cmpeq_epi16(clamp_value, zero);
 
-#ifndef __SSE4_1__
-  neg_val = _mm_and_si128(neg_check, neg_val);
-  pos_val = _mm_andnot_si128(neg_check, pos_val);
-  return _mm_or_si128(neg_val, pos_val);
-#else
-  return _mm_blendv_epi8(pos_val, neg_val, neg_check);
-#endif
+  return _mm_blendv_epi8(clamp_value, acc_lo, clamp_match_mask);
 }
 
