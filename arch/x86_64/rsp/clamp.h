16a7c434da35e15415350ecc0c899441984ebb7a
Tyler Stachecki <tstache1@binghamton.edu>
2014-11-05 16:58:59 -0500

    Fix/optimize the RSP accumulator clamp LO algorithm.

--- arch/x86_64/rsp/clamp.h
+++ arch/x86_64/rsp/clamp.h
@@ -7,20 +7,30 @@
 
 static inline __m128i rsp_clamp_lo(__m128i acc_lo,
   __m128i acc_md, __m128i acc_hi, __m128i zero) {
-  __m128i clamp_match_mask, sign_match_mask;
-  __m128i lo, hi, md_clamped, clamp_value;
+  __m128i unclamped_val, mid_sign, neg_check, zero_check;
+  __m128i clamped_val, clamp_mask;
 
-  lo = _mm_unpacklo_epi16(acc_md, acc_hi);
-  hi = _mm_unpackhi_epi16(acc_md, acc_hi);
-  md_clamped = _mm_packs_epi32(lo, hi);
+  zero_check = _mm_cmpeq_epi16(acc_hi, zero);
+  neg_check = _mm_cmplt_epi16(acc_hi, zero);
+  mid_sign = _mm_srai_epi16(acc_md, 15);
+  unclamped_val = acc_lo;
 
-  clamp_match_mask = _mm_cmpeq_epi16(md_clamped, acc_md);
-  clamp_value = _mm_srai_epi16(md_clamped, 15);
+  clamped_val = _mm_cmpeq_epi16(neg_check, zero);
 
-  sign_match_mask = _mm_cmpeq_epi16(clamp_value, acc_hi);
-  clamp_match_mask = _mm_and_si128(clamp_match_mask, sign_match_mask);
-  clamp_value = _mm_cmpeq_epi16(clamp_value, zero);
+#ifndef __SSE4_1__
+  neg_check = _mm_and_si128(mid_sign, neg_check);
+  zero_check = _mm_andnot_si128(mid_sign, zero_check);
+  clamp_mask = _mm_or_si128(neg_check, zero_check);
+#else
+  clamp_mask = _mm_blendv_epi8(zero_check, neg_check, mid_sign);
+#endif
 
-  return _mm_blendv_epi8(clamp_value, acc_lo, clamp_match_mask);
+#ifndef __SSE4_1__
+  unclamped_val = _mm_and_si128(clamp_mask, unclamped_val);
+  clamped_val = _mm_andnot_si128(clamp_mask, clamped_val);
+  return _mm_or_si128(unclamped_val, clamped_val);
+#else
+  return _mm_blendv_epi8(clamped_val, unclamped_val, clamp_mask);
+#endif
 }
 
