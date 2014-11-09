4cfb7275a9ed1792b1af00a5188ab3d512b299ce
Tyler Stachecki <tstache1@binghamton.edu>
2014-11-08 19:07:08 -0500

    Fix and optimize rsp_uclamp_acc (once again).

--- arch/x86_64/rsp/clamp.h
+++ arch/x86_64/rsp/clamp.h
@@ -14,22 +14,22 @@ static inline __m128i rsp_sclamp_acc_tomd(
 
 static inline __m128i rsp_uclamp_acc(__m128i val,
   __m128i acc_md, __m128i acc_hi, __m128i zero) {
-  __m128i mid_sign, neg_check, zero_check;
-  __m128i clamped_val, clamp_mask;
+  __m128i clamp_mask, clamped_val;
+  __m128i mid_sign, hi_negative;
 
-  zero_check = _mm_cmpeq_epi16(acc_hi, zero);
-  neg_check = _mm_cmplt_epi16(acc_hi, zero);
+  hi_negative = _mm_cmplt_epi16(acc_hi, zero);
   mid_sign = _mm_srai_epi16(acc_md, 15);
 
-  clamped_val = _mm_cmpeq_epi16(neg_check, zero);
+  // We don't have to clamp if the HI part of the
+  // accumulator is sign-extended down to the MD part.
+  clamp_mask = _mm_cmpeq_epi16(mid_sign, acc_hi);
 
-#ifndef __SSE4_1__
-  neg_check = _mm_and_si128(mid_sign, neg_check);
-  zero_check = _mm_andnot_si128(mid_sign, zero_check);
-  clamp_mask = _mm_or_si128(neg_check, zero_check);
-#else
-  clamp_mask = _mm_blendv_epi8(zero_check, neg_check, mid_sign);
-#endif
+  // Generate the value in the event we need to clamp.
+  //   * hi_negative, mid_sign => xxxx
+  //   * hi_negative, !mid_sign => 0000
+  //   * !hi_negative, mid_sign => FFFF
+  //   * !hi_negative, !mid_sign => FFFF
+  clamped_val = _mm_cmpeq_epi16(hi_negative, zero);
 
 #ifndef __SSE4_1__
   val = _mm_and_si128(clamp_mask, val);
