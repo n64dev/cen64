2f0e33263df312a503cfeb710b6779b7564e22ed
Tyler Stachecki <tstache1@binghamton.edu>
2014-11-09 13:20:10 -0500

    Add RSP reciprocal ROM contents.

---
+++ rsp/reciprocal.h
@@ -0,0 +1,18 @@
+//
+// rsp/reciprocal.h: RSP reciprocal ROM contents.
+//
+// CEN64: Cycle-Accurate Nintendo 64 Simulator.
+// Copyright (C) 2014, Tyler J. Stachecki.
+//
+// This file is subject to the terms and conditions defined in
+// 'LICENSE', which is part of this source code package.
+//
+
+#ifndef __rsp_reciprocal_h__
+#define __rsp_reciprocal_h__
+#include "common.h"
+
+extern const uint16_t rsp_reciprocal_rom[1024];
+
+#endif
+
