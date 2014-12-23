ae714715fb44a9d6084451df4323477c4a78b11c
Tyler Stachecki <tstache1@binghamton.edu>
2014-12-23 01:13:50 -0500

    Commit AIO's VABS optimization.

--- arch/x86_64/rsp/vabs.h
+++ arch/x86_64/rsp/vabs.h
@@ -9,16 +9,14 @@
 
 static inline __m128i rsp_vabs(__m128i vs, __m128i vt,
   __m128i zero, __m128i *acc_lo) {
-  __m128i sign_lt = _mm_srai_epi16(vs, 15);
   __m128i vs_zero = _mm_cmpeq_epi16(vs, zero);
-  __m128i vd;
+  __m128i sign_lt = _mm_srai_epi16(vs, 15);
+  __m128i vd = _mm_andnot_si128(vs_zero, vt);
 
   // Careful: if VT = 0x8000 and VS is negative,
   // acc_lo will be 0x8000 but vd will be 0x7FFF.
-  vd = _mm_xor_si128(vt, sign_lt);
+  vd = _mm_xor_si128(vd, sign_lt);
   *acc_lo = _mm_sub_epi16(vd, sign_lt);
-  vd = _mm_subs_epi16(vd, sign_lt);
-  *acc_lo = _mm_andnot_si128(vs_zero, *acc_lo);
-  return _mm_andnot_si128(vs_zero, vd);
+  return _mm_subs_epi16(vd, sign_lt);
 }
 
