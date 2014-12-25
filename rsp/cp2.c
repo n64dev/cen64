03f04c1b8244226f6222c92e96d68a46a199a1cb
Tyler Stachecki <tstache1@binghamton.edu>
2014-12-25 12:09:01 -0500

    Add implementation for MTC2.

--- rsp/cp2.c
+++ rsp/cp2.c
@@ -43,18 +43,47 @@ void RSP_MFC2(struct rsp *rsp,
   uint32_t iw, uint32_t rs, uint32_t rt) {
   struct rsp_exdf_latch *exdf_latch = &rsp->pipeline.exdf_latch;
   const uint16_t *e = rsp->cp2.regs[GET_RD(iw)].e;
-  unsigned dest, element;
+  unsigned dest, element = GET_EL(iw);
   uint8_t low, high;
   uint32_t data;
 
-  element = GET_EL(iw);
+  unsigned lo = element >> 1;
+  unsigned hi;
+
   dest = GET_RT(iw);
 
-  low = e[element >> 1] >> ((element & 0x1) << 3);
-  high = e[(element + 1) >> 1] >> (((element + 1) & 0x1) << 3);
-  data = (int16_t) ((high << 8) | low);
+  if (unlikely(element & 0x1)) {
+    hi = (element + 1) >> 1;
+
+    low = e[lo] >> ((element & 0x1) << 3);
+    high = e[hi] >> (((element + 1) & 0x1) << 3);
+    data = (int16_t) ((high << 8) | low);
+  }
+
+  else
+    data = (int16_t) e[lo];
 
   exdf_latch->result.result = data;
   exdf_latch->result.dest = dest;
+}
+
+//
+// MTC2
+//
+void RSP_MTC2(struct rsp *rsp,
+  uint32_t iw, uint32_t rs, uint32_t rt) {
+  uint16_t *e = rsp->cp2.regs[GET_RD(iw)].e;
+  unsigned element = GET_EL(iw);
+
+  if (unlikely(element & 0x1)) {
+    unsigned lo = element >> 1;
+    unsigned hi = (element + 1) >> 1;
+
+    e[lo] = (e[lo] & 0x00FF) | ((rt & 0xFF) << 8);
+    e[hi] = (e[hi] & 0xFF00) | (rt >> 8 & 0xFF);
+  }
+
+  else
+    e[element] = rt;
 }
 
