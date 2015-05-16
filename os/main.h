58c1a0e9f1368ec33ce49b181b0712eff63fcadb
Tyler J. Stachecki <tstache1@cs.binghamton.edu>
2015-05-16 13:16:06 -0400

    More cleanup/streamlining of UI pullup.

--- os/main.h
+++ os/main.h
@@ -14,15 +14,5 @@
 #include "os/gl_window.h"
 #include "rom_file.h"
 
-cen64_cold int os_main(struct cen64_device *device,
-  struct cen64_options *options);
-
-cen64_cold bool os_exit_requested(struct gl_window *gl_window);
-cen64_cold void os_render_frame(cen64_gl_window window, const void *data,
-  unsigned xres, unsigned yres, unsigned xskip, unsigned type);
-
-cen64_cold void os_acquire_input(struct gl_window *gl_window);
-cen64_cold void os_release_input(struct gl_window *gl_window);
-
 #endif
 
