9ab0fe066038d9655daa87443349b81ddc9e3385
Mike Ryan <mikeryan@lacklustre.net>
2016-01-26 22:15:38 -0800

    validate SHA1 of important files
    Currently only validating pifdata.bin. Probably also worth validating DD
    IPL and other known binaries.

---
+++ device/sha1_sums.h
@@ -0,0 +1,19 @@
+//
+// device/sha1_sums.h: Known good SHA1 sums
+//
+// CEN64: Cycle-Accurate Nintendo 64 Emulator.
+// Copyright (C) 2015, Tyler J. Stachecki.
+//
+// This file is subject to the terms and conditions defined in
+// 'LICENSE', which is part of this source code package.
+//
+
+#ifndef __SHA1_SUMS_H__
+#define __SHA1_SUMS_H__
+
+const uint8_t sha1_pifrom_ntsc[SHA1_SIZE] = {
+  0x91, 0x74, 0xea, 0xdc, 0x0f, 0x0e, 0xa2, 0x65, 0x4c, 0x95,
+  0xfd, 0x94, 0x14, 0x06, 0xab, 0x46, 0xb9, 0xdc, 0x9b, 0xdd,
+};
+
+#endif
