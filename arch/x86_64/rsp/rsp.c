3003d774cb52cb8c55fb7a46812ef217842cbaa3
Tyler J. Stachecki <tstache1@binghamton.edu>
2016-02-06 13:58:04 -0500

    Improved SSE2 vector shuffle patch from izy.

--- arch/x86_64/rsp/rsp.c
+++ arch/x86_64/rsp/rsp.c
@@ -233,49 +233,61 @@ int arch_rsp_init(struct rsp *rsp) { return 0; }
 #ifndef __SSSE3__
 __m128i rsp_vect_load_and_shuffle_operand(
   const uint16_t *src, unsigned element) {
-  uint16_t word_lo, word_hi;
-  uint64_t dword;
+  __m128i v;
 
-  // element => 0w ... 7w
-  if (element >= 8) {
-    memcpy(&word_lo, src + (element - 8), sizeof(word_lo));
-    dword = word_lo | ((uint32_t) word_lo << 16);
+  switch(element) {
+    case 0:
+    case 1:
+      v = _mm_load_si128((__m128i *) src);
+      return v;
 
-    return _mm_shuffle_epi32(_mm_loadl_epi64((__m128i *) &dword),
-      _MM_SHUFFLE(0,0,0,0));
-  }
-
-  // element => 0h ... 3h
-  else if (element >= 4) {
-    __m128i v;
-
-    memcpy(&word_hi, src + element - 0, sizeof(word_hi));
-    memcpy(&word_lo, src + element - 4, sizeof(word_lo));
-    dword = word_lo | ((uint32_t) word_hi << 16);
-
-    v = _mm_loadl_epi64((__m128i *) &dword);
-    v = _mm_shufflelo_epi16(v, _MM_SHUFFLE(1,1,0,0));
-    return _mm_shuffle_epi32(v, _MM_SHUFFLE(1,1,0,0));
-  }
-
-  // element => 0q ... 1q
-  else if (element >= 2) {
-    __m128i v = rsp_vect_load_unshuffled_operand(src);
-
-    if (element == 2) {
+    // element => 0q
+    case 2:
+      v = _mm_load_si128((__m128i *) src);
       v = _mm_shufflelo_epi16(v, _MM_SHUFFLE(2,2,0,0));
       v = _mm_shufflehi_epi16(v, _MM_SHUFFLE(2,2,0,0));
-    }
+      return v;
 
-    else {
+    // element => 1q
+    case 3:
+      v = _mm_load_si128((__m128i *) src);
       v = _mm_shufflelo_epi16(v, _MM_SHUFFLE(3,3,1,1));
       v = _mm_shufflehi_epi16(v, _MM_SHUFFLE(3,3,1,1));
-    }
+      return v;
 
-    return v;
+    // element => 0h ... 3h
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+      __asm__("" : "=x"(v)); /* Do not remove. */
+      v = _mm_insert_epi16(v, src[element - 4], 0);
+      v = _mm_insert_epi16(v, src[element - 0], 1);
+      v = _mm_shufflelo_epi16(v, _MM_SHUFFLE(1,1,0,0));
+      v = _mm_shuffle_epi32(v, _MM_SHUFFLE(1,1,0,0));
+      return v;
+
+    // element => 0w ... 7w
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+      __asm__("" : "=x"(v)); /* Do not remove. */
+      v = _mm_insert_epi16(v, src[element - 8], 0);
+      v = _mm_unpacklo_epi16(v, v);
+      v = _mm_shuffle_epi32(v, _MM_SHUFFLE(0,0,0,0));
+      return v;
   }
 
-  return rsp_vect_load_unshuffled_operand(src);
+  #ifdef NDEBUG
+  __builtin_unreachable();
+  #else
+  __builtin_trap();
+  #endif
 }
 #endif
 
