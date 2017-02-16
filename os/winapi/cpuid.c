ac8f2283afaafcbe6a023663e5ee190529f376af
Tyler J. Stachecki <stachecki.tyler@gmail.com>
2017-02-15 19:04:53 -0500

    Use a different version of cpuid functions.

--- os/winapi/cpuid.c
+++ os/winapi/cpuid.c
@@ -14,7 +14,16 @@
 void cen64_cpuid(uint32_t eax, uint32_t ecx, struct cen64_cpuid_t *cpuid) {
   int cpuInfo[4];
 
+#ifdef _MSC_VER
   __cpuidex(cpuInfo, eax, ecx);
+#else
+  __asm__ __volatile__(
+    "cpuid\n\t"
+
+    : "=a"(cpuInfo[0]), "=b"(cpuInfo[1]), "=c"(cpuInfo[2]), "=d"(cpuInfo[3])
+    : "0"(eax), "2"(ecx)
+  );
+#endif
 
   cpuid->eax = cpuInfo[0];
   cpuid->ebx = cpuInfo[1];
@@ -25,7 +34,16 @@ void cen64_cpuid(uint32_t eax, uint32_t ecx, struct cen64_cpuid_t *cpuid) {
 void cen64_cpuid_get_vendor(char vendor[13]) {
   int cpuInfo[4];
 
+#ifdef _MSC_VER
   __cpuidex(cpuInfo, 0, 0);
+#else
+  __asm__ __volatile__(
+    "cpuid\n\t"
+
+    : "=a"(cpuInfo[0]), "=b"(cpuInfo[1]), "=c"(cpuInfo[2]), "=d"(cpuInfo[3])
+    : "0"(0), "2"(0)
+  );
+#endif
 
   memcpy(vendor + 0, cpuInfo + 1, sizeof(*cpuInfo));
   memcpy(vendor + 4, cpuInfo + 3, sizeof(*cpuInfo));
