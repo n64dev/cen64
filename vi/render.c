ed14563b0e70d20cfc0241b0b96e82fb86676067
PeterLemon <krom@gbadev.org>
2018-04-29 20:22:15 +0100

    set texture wrap s & t to clamp instead of repeat

--- vi/render.c
+++ vi/render.c
@@ -26,8 +26,8 @@ void gl_window_init(struct vi_controller *vi) {
   // Initialize the texture that we'll use for drawing the screen.
   glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 
   // Initialize vertex arrays for drawing.
   glEnableClientState(GL_VERTEX_ARRAY);
