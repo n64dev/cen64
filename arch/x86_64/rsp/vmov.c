3b879e32d7063b4b83867fac9a8c38f042a0fc2b
pseudophpt <pseudophpt@gmail.com>
2018-09-03 22:54:57 -0400

    Remove debug information from VMOV code

--- arch/x86_64/rsp/vmov.c
+++ arch/x86_64/rsp/vmov.c
@@ -16,9 +16,6 @@ __m128i rsp_vmov(struct rsp *rsp,
   // Copy element into data
   memcpy(&data, (e & 0x7) + (uint16_t *)&vt_shuffle, sizeof(uint16_t));
 
-  printf("src %d dest %d el %x data %x\n", src, dest, e, data);
-  fflush(stdout);
-
   // Write out the upper part of the result.
   rsp->cp2.regs[dest].e[e & 0x7] = data;
   return rsp_vect_load_unshuffled_operand(rsp->cp2.regs[dest].e);
